99c21a6f575ea5ddc78b255be04cd8243056fa22
Christina <Christinepeninsula@hotmail.com>
2019-04-13 19:55:30 -0400

    psycopg2-binary

--- interview/models.py
+++ interview/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Interview(models.Model):
-    picture = models.ImageField(null=True)
+    picture = models.ImageField(null=True, upload_to='media')
     subjectName = models.CharField(max_length=255, null=True)
     title = models.CharField(max_length=255, null=True)
     body = models.TextField(null=True)
