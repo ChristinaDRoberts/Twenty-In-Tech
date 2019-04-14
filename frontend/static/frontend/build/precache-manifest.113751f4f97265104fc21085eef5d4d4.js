4c082ec6dc95cdcf936cca5f666cc0e7a78e7433
Christina <Christinepeninsula@hotmail.com>
2019-04-14 16:53:52 -0400

    added frontend directory inside static per django docs

---
+++ frontend/static/frontend/build/precache-manifest.113751f4f97265104fc21085eef5d4d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a71ebe21ccfa9eeefd7",
+    "url": "/static/css/main.ac9b65cc.chunk.css"
+  },
+  {
+    "revision": "0a71ebe21ccfa9eeefd7",
+    "url": "/static/js/main.0db305ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e99bfa42f06aabdbaf4",
+    "url": "/static/js/2.0bb57879.chunk.js"
+  },
+  {
+    "revision": "a0cc450ecb2fc0d32195d81d9d656523",
+    "url": "/index.html"
+  }
+];
