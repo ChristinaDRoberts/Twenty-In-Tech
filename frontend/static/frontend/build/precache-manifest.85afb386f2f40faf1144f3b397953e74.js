15ef2611669c87266e1975c4b65d8da760d6bdea
Christina <Christinepeninsula@hotmail.com>
2019-04-14 20:51:55 -0400

    h

---
+++ frontend/static/frontend/build/precache-manifest.85afb386f2f40faf1144f3b397953e74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dcb9161eff00fb62f2c",
+    "url": "/static/css/main.7acdbd12.chunk.css"
+  },
+  {
+    "revision": "5dcb9161eff00fb62f2c",
+    "url": "/static/js/main.3567a9c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7fd8e4fcba6c7bfba062",
+    "url": "/static/js/2.0ff21cb3.chunk.js"
+  },
+  {
+    "revision": "79fcbb4b515ef4bee3c8183d7b102a3b",
+    "url": "/index.html"
+  }
+];
